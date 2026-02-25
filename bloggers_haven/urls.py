601348761c6ed322f238124c8d4499e9ff59b5f0
Janaki Rai <raijanaki68@gmail.com>
2026-02-25 20:18:57 +0545

    Fixed MEDIA_ROOT typo

--- bloggers_haven/urls.py
+++ bloggers_haven/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$',serve, {'document_root':settings.MEIDA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$',serve, {'document_root':settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$',serve, {'document_root':settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
